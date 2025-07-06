94a832c791ac5878313393d74c629de05aebfbe3
Madhav <spmadhav04@gmail.com>
2025-07-06 16:59:59 +0530

    Update axiosInstance.js

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: https://mern-ecommerce-backend-nqxm.onrender.com/api/v1",
+  baseURL: "https://mern-ecommerce-backend-nqxm.onrender.com/api/v1",
   withCredentials: true, // ✅ Send cookies automatically
 });
 
